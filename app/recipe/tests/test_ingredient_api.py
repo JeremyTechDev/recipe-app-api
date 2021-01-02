b9f79bdecc465d84defdca613706a74772537c49
Jeremy <jeremy2918@gmail.com>
2021-01-02 13:20:36 -0400

    Added ingredient feature

--- app/recipe/tests/test_ingredient_api.py
+++ app/recipe/tests/test_ingredient_api.py
@@ -65,3 +65,20 @@ class TestIngredientPrivateAPI(TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient(self):
+        """Tests creaing a new ingredient"""
+        payload = {'name': 'Cabbage'}
+        self.client.post(INGREDIENT_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user, name=payload['name']).exists
+
+        self.assertTrue(exists)
+
+    def test_create_invalid_ingredient(self):
+        """Test not creating a ingredient with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENT_URL, payload)
+
+        self.assertEqual(res.status_code, 400)
